9630e76bfa80a8605404c0c57e2f64e4ab0cc437
Nestor Rubio <nestordrubio9@gmail.com>
2024-01-08 23:46:27 -0500

    added dev experiences

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -1,14 +1,60 @@
 import { useTheme } from "../components/ThemeContext";
+import { experiences } from "../utils/utils";
 
 const Experience = () => {
   const {isDarkMode} = useTheme();
   return (
     <div id="experience" className={`${isDarkMode ? 'dark' : ''}`}>
       <div className="bg-lightBG1 dark:bg-darkBG1 flex flex-col justify-center py-[24px]">
-        <p className="self-center text-lightText dark:text-darkText text-[24px] font-bold">Experience</p>
+        <p className="self-center text-lightText dark:text-darkText text-[24px] font-bold mb-[24px]">Dev Experience</p>
+        <div className="flex flex-col justify-center self-center gap-2">
+        {
+          experiences.map(item => (
+            ExpItem(item)
+          ))
+        }
+        </div>
       </div> 
     </div>
   )
 };
+
+const ExpItem = (
+  exp:{
+    title: string
+    company: string
+    location: string
+    logo: any
+    dateRange: string
+    description: string[]
+    techUsed: string[]
+  }) => {
+    return (
+      <div className="flex flex-row space-x-[25px] w-[90%] relative self-center">
+        <div className="absolute h-full border-l border-lightText dark:border-darkText rounded"/>
+        <div>
+          <p className="text-xs text-lightText dark:text-darkText italic">{exp.dateRange}</p>
+          <p className="text-xl text-lightText dark:text-darkText font-bold">{exp.title}</p>
+          <p className="text-sm text-lightText dark:text-darkText italic">{exp.company}</p>
+          <p className="text-sm text-lightText dark:text-darkText italic">{exp.location}</p>
+          <ul className="list-disc mt-2">
+            {
+              exp.description.map(str => (
+                <li className="text-xs text-lightText dark:text-darkText">{str}</li>
+              ))
+            }
+          </ul>
+          <div className="flex flex-row space-x-2 flex-wrap mt-2">
+          {
+            exp.techUsed.map(str => (
+              <p className="cursor-default text-xs p-2 bg-lightAccent dark:bg-darkAccent rounded-full text-lightText dark:text-darkText mb-2">{str}</p>
+            ))
+          }
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
 
 export default Experience;
--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -80,7 +80,12 @@ export const experiences = [
     location: "Orlando, FL",
     logo: WorldLogo,
     dateRange: "July 2023 - Present",
-    description: "",
+    description: [
+      "Drive the development and enhancement of Milly's web and mobile applications by leveraging extensive expertise in React and React Native, while also crafting compelling demo products showcased to potential clients",
+      "Lead Git version control management and actively mentor colleagues on diverse aspects of project execution and technology",
+      "Lead and guide the design phases across all products, playing a pivotal role in ensuring high-quality design standards and user experience",
+      "Undertake a leadership role in the conceptualization and implementation of innovative design processes to enhance product development and usability"
+    ],
     techUsed: ["React","React Native","Expo Go","Tailwind","Redux Toolkit","Figma"],
   },
   {
@@ -89,7 +94,12 @@ export const experiences = [
     location: "Seattle, WA",
     logo: QualtricsLogo,
     dateRange: "May 2022 - August 2022",
-    description: "",
+    description: [
+      "Leveraged Go, Docker, Jenkins, and Bash scripting in an ambitious initiative to overhaul the Load Balancer as Code repository merge validation, resulting in a notable 45% reduction in the runtime of the verification script",
+      "Demonstrated adeptness with Golang's Testify Suite to uphold the quality of code changes, ensuring robust and reliable enhancements to the project",
+      "Championed comprehensive documentation efforts, streamlining project maintenance and ensuring ease of future development and scalability",
+      "Embraced a steep learning curve, swiftly mastering previously unfamiliar technologies to achieve impactful improvements in project efficiency and effectiveness"
+    ],
     techUsed: ["Go","Docker","Jenkins","Gitlab","Cobra CLI"]
   },
   {
@@ -98,7 +108,12 @@ export const experiences = [
     location: "Orlando, FL",
     logo: UCFLogo,
     dateRange: "May 2021 - August 2023",
-    description: "",
+    description: [
+      "Hosted extensive tutoring sessions benefiting hundreds of students enrolled in courses covering Object-Oriented Programming w/ Java and Computer Science I w/ C, encompassing topics such as data structures, algorithms, and both object-oriented and procedural programming",
+      "Earned numerous positive reviews in voluntary surveys, reflecting my dedication, empathy, and commitment to aiding students, culminating in top-ranking departmental feedback over multiple semesters",
+      "Promoted to the role of Peer Tutor Mentor in November 2021, enabling me to supervise and offer constructive feedback to fellow tutors, contributing insights to enhance their sessions while reporting observations to the Peer Tutoring Coordinator",
+      "Effectively imparted practical skills extending beyond the academic curriculum, including Git, JUnit Testing, Valgrind Memory Leak testing for C programs, and proficiently navigating technical documentation"
+    ],
     techUsed: ["Java","C","Python","WordPress"]
   },
 ]
