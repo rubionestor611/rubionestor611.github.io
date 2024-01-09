4b9c7836738b5654f5dc4971f4bb5662f11e4b09
Nestor Rubio <nestordrubio9@gmail.com>
2024-01-08 22:27:50 -0500

    lot of pngs added for skills

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -9,22 +9,33 @@ const Skills = () => {
       <div className="bg-lightBG1 dark:bg-darkBG1 flex flex-col justify-center py-[24px]">
         <p className="self-center text-lightText dark:text-darkText text-[24px] font-bold">Skills</p>
         <p className="self-center text-lightText dark:text-darkText text-[20px] font-semibold">Programming Languages</p>
-        <div className="pt-1 self-center flex flex-col justify-center gap-2">
-          {
-            skills.languages.map((lang) => (
-              <div
-                className="group flex flex-col hover:scale-125 hover:my-[20px] cursor-default bg-lightBG2 dark:bg-darkBG2 text-lightText dark:text-darkText rounded h-[100px] w-[200px] text-xl font-bold border-2 border-lightText dark:border-darkText items-center justify-center"
-              >
-                <img src={lang.logo} alt={`${lang.name} logo`} className="h-[50px] w-[50px]"/>
-                <p>{lang.name}</p>
-              </div>
-            ))
-          }
+        <div className="pt-1 self-center flex flex-row flex-wrap justify-center gap-2 md:gap-4 lg:gap-8 w-[90%]">
+        {
+          skills.languages.map((lang) => (
+            TechCard(lang)
+          ))
+        }
         </div>
         <p className="self-center text-lightText dark:text-darkText text-[20px] font-semibold">Tools & Frameworks</p>
+        <div className="pt-1 self-center flex flex-row flex-wrap justify-center gap-2 md:gap-4 lg:gap-8 w-[90%]">
+        {
+          skills.technologies.map((lang) => (
+            TechCard(lang)
+          ))
+        }
+        </div>
       </div> 
     </div>
   )
 };
+
+const TechCard = (lang:{name:string, logo:any}) => {
+  return (
+    <div className="group flex flex-col hover:scale-125 hover:my-[20px] md:hover:my-0 cursor-default bg-lightBG2 dark:bg-darkBG2 text-lightText dark:text-darkText rounded h-[100px] w-[200px] text-xl font-bold border-2 border-lightText dark:border-darkText items-center justify-center">
+      <img src={lang.logo} alt={`${lang.name} logo`} className="h-[50px] w-[50px] object-contain"/>
+      <p>{lang.name}</p>
+    </div>
+  );
+}
 
 export default Skills;
--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -6,6 +6,28 @@ import JavaScriptLogo from "../assets/tech/js.png";
 import WorldLogo from "../assets/exp/worldwide.png";
 import UCFLogo from "../assets/exp/ucf.png";
 import QualtricsLogo from "../assets/exp/qualtrics.png";
+import HTMLLogo from "../assets/tech/html.png";
+import CSSLogo from "../assets/tech/css.png";
+import ReactLogo from "../assets/tech/react.png";
+import TypeScriptLogo from "../assets/tech/typescript.png";
+import TailwindLogo from "../assets/tech/tailwind.svg";
+import ReduxLogo from "../assets/tech/redux.png";
+import ExpressLogo from "../assets/tech/express.png";
+import NodeLogo from "../assets/tech/nodejs.png";
+import JUnitLogo from "../assets/tech/junit.png";
+import TestifyLogo from "../assets/tech/Testify.png";
+import PostmanLogo from "../assets/tech/postman.svg";
+import AzureLogo from "../assets/tech/azure.png";
+import MongoLogo from "../assets/tech/mongodb.png";
+import MySQLLogo from "../assets/tech/mysql.png";
+import FirebaseLogo from "../assets/tech/firebase.png";
+import DockerLogo from "../assets/tech/docker.png";
+import BootstrapLogo from "../assets/tech/bootstrap.png";
+import JenkinsLogo from "../assets/tech/jenkins.png";
+import GitLogo from "../assets/tech/git.svg";
+import GitlabLogo from "../assets/tech/gitlabLogo.svg";
+import GithubLogo from "../assets/tech/githubLogo.png";
+
 export const navLinks = [
   ['Home','#home'],
   ['Skills','#skills'],
@@ -25,28 +47,29 @@ export const skills = {
     {name: "Go", logo: GoLogo},
   ],
   technologies: [
-    {name:"React",logo:""},
-    {name:"React Native",logo:""},
-    {name:"TypeScript",logo:""},
-    {name:"Tailwind CSS",logo:""},
-    {name:"Redux Toolkit",logo:""},
-    {name:"ExpressJS",logo:""},
-    {name:"NodeJS",logo:""},
-    {name:"SQL",logo:""},
-    {name:"MySQL",logo:""},
-    {name:"MongoDB",logo:""},
-    {name:"Google Firebase",logo:""},
-    {name:"Bootstrap",logo:""},
-    {name:"Git",logo:""},
-    {name:"GitLab",logo:""},
-    {name:"Github",logo:""},
-    {name:"Docker",logo:""},
-    {name:"Jenkins",logo:""},
-    {name:"Azure",logo:""},
-    {name:"Java Swing",logo:""},
-    {name:"Postman",logo:""},
-    {name:"Golang Testify",logo:""},
-    {name:"JUnit",logo:""},
+    {name:"React",logo:ReactLogo},
+    {name:"React Native",logo:ReactLogo},
+    {name:"TypeScript",logo:TypeScriptLogo},
+    {name:"Tailwind CSS",logo:TailwindLogo},
+    {name:"Redux Toolkit",logo:ReduxLogo},
+    {name:"ExpressJS",logo:ExpressLogo},
+    {name:"NodeJS",logo:NodeLogo},
+    {name:"HTML",logo:HTMLLogo},
+    {name:"CSS",logo:CSSLogo},
+    {name:"MySQL",logo:MySQLLogo},
+    {name:"MongoDB",logo:MongoLogo},
+    {name:"Google Firebase",logo:FirebaseLogo},
+    {name:"Bootstrap",logo:BootstrapLogo},
+    {name:"Git",logo:GitLogo},
+    {name:"GitLab",logo:GitlabLogo},
+    {name:"Github",logo:GithubLogo},
+    {name:"Docker",logo:DockerLogo},
+    {name:"Jenkins",logo:JenkinsLogo},
+    {name:"Azure",logo:AzureLogo},
+    {name:"Java Swing",logo:JavaLogo},
+    {name:"Postman",logo:PostmanLogo},
+    {name:"Golang Testify",logo:TestifyLogo},
+    {name:"JUnit",logo:JUnitLogo},
   ]
 }
 
